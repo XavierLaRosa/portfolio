06c76b431ff5b3f49e74af3a2d714446dec5c1a0
Xavier La Rosa <xavier.a.larosa@gmail.com>
2019-12-19 19:43:17 -0500

    working on refactoring code base into Angular project
    Updates:
    Angular version can hold font awesome icons, all major components are covnerted to Angular components, need to make components like carousel projects and modals dynamic components, need new gifs for corrupted files

---
+++ refactor/portfolio-angular-version/src/app/app.module.ts
@@ -0,0 +1,68 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { GeneralBioComponent } from "./general-bio/general-bio.component";
+import { MainSkillsComponent } from "./main-skills/main-skills.component";
+import { ProjectCarouselComponent } from "./project-carousel/project-carousel.component";
+import { SkillsAnalysisComponent } from "./skills-analysis/skills-analysis.component";
+import { ChartsModule } from "ng2-charts";
+import {
+  FontAwesomeModule,
+  FaIconLibrary
+} from "@fortawesome/angular-fontawesome";
+import {
+  faLinkedin,
+  faLinkedinIn,
+  faGithub,
+  faGithubAlt,
+  faGithubSquare,
+  faReact,
+  faAngular,
+  faSwift,
+  faJava
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faNewspaper,
+  faPaintBrush,
+  faAddressBook,
+  faGraduationCap,
+  faBriefcase,
+  faUsers
+} from "@fortawesome/free-solid-svg-icons";
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    GeneralBioComponent,
+    MainSkillsComponent,
+    ProjectCarouselComponent,
+    SkillsAnalysisComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, ChartsModule, FontAwesomeModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(
+      faLinkedinIn,
+      faLinkedin,
+      faGithub,
+      faGithubAlt,
+      faGithubSquare,
+      faReact,
+      faAngular,
+      faSwift,
+      faJava,
+      faNewspaper,
+      faAddressBook,
+      faPaintBrush,
+      faGraduationCap,
+      faBriefcase,
+      faUsers
+    );
+  }
+}
---
+++ refactor/portfolio-angular-version/src/app/general-bio/general-bio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-general-bio',
+  templateUrl: './general-bio.component.html',
+  styleUrls: ['./general-bio.component.css']
+})
+export class GeneralBioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
